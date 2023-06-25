7cba23d190171120a4a541d4613e053196f0b0ef
satyamKumar <satyamkumar2302@gmail.com>
2023-06-25 19:58:34 +0530

    setup basic sprinboot project

---
+++ src/test/java/io/github/zerothreadsonfire/springbootplayground/SpringbootPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.zerothreadsonfire.springbootplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
